c61bef3147bbe169b4aba1d430c3579115c52601
Alexander Volf <firewolf_ttyd0@mail.ru>
2018-06-17 13:10:53 +0300

    Update test script

--- scripts/timing-service-test.sh
+++ scripts/timing-service-test.sh
@@ -4,7 +4,19 @@ TOKEN=`cat authtoken.data | grep "id_token" | awk -F '"' '{print $4}'`
 
 echo $TOKEN
 
+rm result.data
+
 curl -v -c cookies.txt -b cookies.txt -X POST  -H 'Accept: application/json, text/plain, */*'  \
 -H 'Connection: keep-alive' -H 'Content-Type: application/json' \
 -H "Authorization: Bearer $TOKEN" -d '[4,3,2,1]' \
- 'http://127.0.0.1:8081/api/ext/route-timing'
+ 'http://127.0.0.1:8081/api/ext/route-timing' > result.data
+
+echo ""
+echo " === RESULT === "
+echo ""
+cat result.data
+echo ""
+echo ""
+echo " DONE "
+
+rm result.data
